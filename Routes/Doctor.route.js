0922487c9401f596d5e562cd4f81bc2f4b68ec88
Himanshu <119741346+himanshuchitoria@users.noreply.github.com>
2025-09-16 12:03:18 +0530

    Add files via upload

---
+++ Routes/Doctor.route.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const {
+  register,
+  login,
+  deleteDoctor,
+  updateDoctor,
+  getAllDoctors,
+  updateAppointment,
+  findDoctor
+} = require("../Controllers/Doctor.controller");
+const DoctorRouter = express.Router();
+const Auth = require("../Middlewares/JWT.authentication");
+const { DoctorAuth } = require("../Middlewares/RoleBased.authentication");
+
+// Doctor Registration
+DoctorRouter.post("/register", register);
+
+// Doctor Login
+DoctorRouter.post("/login", login);
+
+// Doctor Deletion
+DoctorRouter.delete("/:doctorId", Auth, DoctorAuth, deleteDoctor);
+
+// Doctor Update
+DoctorRouter.patch("/:doctorId", Auth, DoctorAuth, updateDoctor);
+
+// All Doctors Data
+DoctorRouter.get("/all", Auth, getAllDoctors);
+
+// find Doctor by id (FIXED!)
+DoctorRouter.get("/:doctorId", Auth, DoctorAuth, findDoctor);
+
+// only do changes in appoinment
+DoctorRouter.patch("/appoinment/:doctorId", Auth, DoctorAuth, updateAppointment);
+
+module.exports = DoctorRouter;
